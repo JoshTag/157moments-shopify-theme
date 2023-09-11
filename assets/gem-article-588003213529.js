f408c834055656e7b5de23aeda6ada360cdb1550
Joshua Taguicana <joshtaguicana@gmail.com>
2023-09-11 13:33:05 -0400

    inital commit for 157moments Shopify Store

---
+++ assets/gem-article-588003213529.js
@@ -0,0 +1,30 @@
+
+
+    (function( jQuery ){
+  var $module = jQuery('#m-1672790790829').children('.module');
+  $module.gfV2HeroBanner({});
+  
+})( window.GemQuery || jQuery );
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
